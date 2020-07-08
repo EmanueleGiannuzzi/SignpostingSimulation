4a2f5b02a2a5b27f8636f38dc5333c2e8e314208
Jan <emagiannu@yahoo.it>
2020-07-08 09:40:22 +0200

    SpawnAreas

---
+++ Assets/Scripts/Simulation/AgentCollisionDetection.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.AI;
+
+[System.Serializable]
+public class CollisionEvent : UnityEvent<NavMeshAgent, Collider> { }
+
+public class AgentCollisionDetection : MonoBehaviour {
+    public CollisionEvent collisionEvent;
+    public Collider destination;
+
+    void Start() {
+        if(collisionEvent == null)
+            collisionEvent = new CollisionEvent();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        collisionEvent.Invoke(this.gameObject.GetComponent<NavMeshAgent>(), other);
+
+        if(destination.Equals(other)) {
+            Destroy(this.gameObject);
+        }
+
+        //if(PedestrianFlow.instances != null) {
+        //    foreach(PedestrianFlow listener in PedestrianFlow.instances) {
+        //        if(listener.isStarted) {
+        //            listener.OnCollidedWithCheckpoint(other.gameObject, this.gameObject);
+        //        }
+        //    }
+        //}
+    }
+}
---
+++ Assets/Scripts/Simulation/SpawnArea.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpawnArea : MonoBehaviour {
+    public Transform destination;
+    public Collider destroyer;
+    public Gradient gradient;
+    public int spawnRate;
+
+    private GameObject agentPrefab;
+
+    void Start() {
+        agentPrefab = this.GetComponentInParent<SpawnAreaSpawner>()._agentPrefab;//TODO: Agent prefab generator
+    }
+
+    public GameObject spawnAgent() {
+        Vector3 position = this.transform.position;
+        Vector3 localScale = this.transform.localScale / 2;
+        float randX = Random.Range(-localScale.x, localScale.x) * 10;
+        float randZ = Random.Range(-localScale.z, localScale.z) * 10;
+
+        Vector3 spawnPoint = new Vector3(position.x + randX, position.y, position.z + randZ);
+        float gradientTime = (Mathf.Sin(Mathf.RoundToInt(Time.fixedTime / Time.fixedDeltaTime)) + 1) / 2;//[0,1]
+        Color agentColor = gradient.Evaluate(gradientTime);
+
+        GameObject agent = Object.Instantiate(agentPrefab, spawnPoint, Quaternion.identity);
+        agent.GetComponent<MeshRenderer>().material.color = agentColor;
+        agent.GetComponent<AgentCollisionDetection>().destination = destroyer;
+
+        NavMeshAgent navMeshAgent = agent.GetComponent<NavMeshAgent>();
+        navMeshAgent.SetDestination(destination.position);
+
+        return agent;
+    }
+}
